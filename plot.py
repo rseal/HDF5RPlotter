a43ccadf76896e1206bad33666bb6e738c255faf
Ryan Seal <ryanseal@zoho.com>
2016-01-23 19:14:57 -0500

    Updated code to use argparse library.

--- plot.py
+++ plot.py
@@ -1,31 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 import matplotlib.pyplot as pp
 import matplotlib.colorbar as cb
 import RadarFigure as rf
 import h5py
-from optparse import OptionParser as op
+import argparse
 import sys
 
 if __name__ == "__main__":
 
 # Main function begins here
-   parser = op()
-   parser.add_option("-f", "--file", dest="fileName", 
-         help="base filename to open")
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--file', dest='fname', help='filename to open')
+   args = parser.parse_args()
 
-   (options, args) = parser.parse_args()
-   fileName = options.fileName
+   if not args.fname:
+      parser.print_help()
 
-   if(fileName == None):
-      print '\n\nERROR: no filename found'
-      print 'usage: plot.py -f filename.h5'
-      sys.exit(2)
-
-   print fileName
+   print args.fname
 
    fapl = h5py.h5p.create(h5py.h5p.FILE_ACCESS)
-   file = h5py.File(fileName, mode='r');
+   file = h5py.File(args.fname, mode='r');
 
    #retrieve sample rate
    pImage = rf.PlotImage(file);
